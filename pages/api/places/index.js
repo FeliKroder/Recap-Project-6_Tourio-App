590015a9b6b84d102423935cc68c936aca2476cd
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-26 11:35:07 +0200

    Using Model: Querying the DB (.findById)

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -3,14 +3,11 @@ import Places from "@/db/models/Places";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
 
   if (request.method === GET) {
-    const places = await Places.findById(id);
-
-    if (!places) {
-      return response.status(404).json({ status: "Not Found!" });
-    }
-    response.status(200).json(places);
+    const place = await Places.find();
+    return response.status(200).json(place);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
   }
 }
