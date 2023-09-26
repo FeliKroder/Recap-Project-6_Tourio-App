9c4257b9c1fa08de8f9e2f9415a5f8ad5a298bfc
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-26 11:23:53 +0200

    Using the Model: Querying the DB (.findById

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,16 @@
-import { places } from "../../../lib/db";
+import dbConnect from "@/db/connect";
+import Places from "@/db/models/Places";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === GET) {
+    const places = await Places.findById(id);
+
+    if (!places) {
+      return response.status(404).json({ status: "Not Found!" });
+    }
+    response.status(200).json(places);
+  }
 }
