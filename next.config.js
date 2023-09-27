1e9329107c507e933b057f5c70a3c145afe906b1
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-27 10:11:19 +0200

    fix next.config.js and ._id in pages/index.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,8 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["images.unsplash.com", "www.google.com"],
+    domains: ["images.unsplash.com", "www.google.com", "unsplash.com"],
   },
 };
+
+module.exports = nextConfig;
